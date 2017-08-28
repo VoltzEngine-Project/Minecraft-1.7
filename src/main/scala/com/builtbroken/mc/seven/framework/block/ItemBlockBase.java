3649e052989ce1cf7f9512ac5f71f0d7fb0a4de9
Robert Seifert <rseifert.phone@gmail.com>
2017-08-28 02:20:08 -0400

    Fixed Json generated item blocks not support json rendering

--- src/main/scala/com/builtbroken/mc/seven/framework/block/ItemBlockBase.java
+++ src/main/scala/com/builtbroken/mc/seven/framework/block/ItemBlockBase.java
@@ -1,13 +1,20 @@
 package com.builtbroken.mc.seven.framework.block;
 
+import com.builtbroken.mc.client.json.ClientDataHandler;
 import com.builtbroken.mc.client.json.IJsonRenderStateProvider;
+import com.builtbroken.mc.client.json.imp.IRenderState;
+import com.builtbroken.mc.client.json.render.RenderData;
 import com.builtbroken.mc.prefab.items.ItemBlockAbstract;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.minecraftforge.client.IItemRenderer;
 
@@ -25,26 +32,6 @@ public class ItemBlockBase extends ItemBlockAbstract implements IJsonRenderState
         super(block);
     }
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public String getRenderContentID(IItemRenderer.ItemRenderType renderType, Object objectBeingRendered)
-    {
-        return getRenderContentID(0);
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public List<String> getRenderContentIDs()
-    {
-        List<String> list = new ArrayList();
-        list.add(getRenderContentID(0));
-        return list;
-    }
-
-    public String getRenderContentID(int meta)
-    {
-        return getBlockBase().getContentID(meta);
-    }
 
     public BlockBase getBlockBase()
     {
@@ -126,5 +113,251 @@ public class ItemBlockBase extends ItemBlockAbstract implements IJsonRenderState
         {
             return false;
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public List<String> getRenderContentIDs()
+    {
+        List<String> list = new ArrayList();
+        list.add(getRenderContentID(0));
+        return list;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public String getRenderContentID(IItemRenderer.ItemRenderType renderType, Object objectBeingRendered)
+    {
+        if (objectBeingRendered instanceof ItemStack)
+        {
+            return getRenderContentID((ItemStack) objectBeingRendered);
+        }
+        else if (objectBeingRendered instanceof Item)
+        {
+            return getRenderContentID(new ItemStack((Item) objectBeingRendered));
+        }
+        else if (objectBeingRendered instanceof Block)
+        {
+            return getRenderContentID(new ItemStack((Block) objectBeingRendered));
+        }
+        return getRenderContentID(0);
+    }
+
+    public String getRenderContentID(ItemStack stack)
+    {
+        return getRenderContentID(stack.getItemDamage());
+    }
+
+    public String getRenderContentID(int meta)
+    {
+        return getBlockBase().getContentID(meta);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean requiresMultipleRenderPasses()
+    {
+        return true; //Fix for getting ItemStack calls
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister reg)
+    {
+        super.registerIcons(reg);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int meta)
+    {
+        return getIconFromState(ClientDataHandler.INSTANCE.getRenderData(getRenderContentID(meta)), meta, 0);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int getRenderPasses(int metadata)
+    {
+        RenderData data = ClientDataHandler.INSTANCE.getRenderData(getRenderContentID(metadata));
+        if (data != null)
+        {
+            return data.itemRenderLayers;
+        }
+
+        return 1;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public int getSpriteNumber()
+    {
+        return super.getSpriteNumber();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamageForRenderPass(int meta, int pass)
+    {
+        return getIconFromState(ClientDataHandler.INSTANCE.getRenderData(getRenderContentID(meta)), meta, pass);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(ItemStack stack, int renderPass, EntityPlayer player, ItemStack usingItem, int useRemaining)
+    {
+        final String contentID = getRenderContentID(stack);
+        final RenderData data = ClientDataHandler.INSTANCE.getRenderData(contentID);
+        if (data != null)
+        {
+            final String renderKey = getRenderKey(stack, player, useRemaining);
+            if (renderKey != null)
+            {
+                IRenderState state = data.getState(RenderData.INVENTORY_RENDER_KEY + "." + renderKey);  //TODO add render pass & use remaining
+                if (state != null)
+                {
+                    IIcon icon = state.getIcon(renderPass);
+                    if (icon != null)
+                    {
+                        return icon;
+                    }
+                }
+            }
+        }
+        return getIcon(stack, renderPass);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(ItemStack stack, int pass)
+    {
+        //Attempt to render using stack -> content ID
+        final String contentID = getRenderContentID(stack);
+        final RenderData data = ClientDataHandler.INSTANCE.getRenderData(contentID);
+        if (data != null)
+        {
+            //Build key set to attempt to get icon TODO cache to improve performance
+            List<String> keys = new ArrayList();
+            String recommendedKey = getRenderKey(stack);
+            if (recommendedKey != null && !recommendedKey.isEmpty())
+            {
+                keys.add(RenderData.INVENTORY_RENDER_KEY + "." + recommendedKey + "." + pass);
+                keys.add(RenderData.INVENTORY_RENDER_KEY + "." + recommendedKey);
+                keys.add(recommendedKey);
+            }
+            keys.add(RenderData.INVENTORY_RENDER_KEY + "." + pass);
+            keys.add(RenderData.INVENTORY_RENDER_KEY);
+
+            //Loop through keys until we find a valid match
+            for (String key : keys)
+            {
+                IRenderState state = data.getState(key);
+                if (state != null)
+                {
+                    if (state != null)
+                    {
+                        IIcon icon = state.getIcon(pass);
+                        if (icon != null)
+                        {
+                            return icon;
+                        }
+                    }
+                }
+            }
+        }
+        //If all else fails fall back to using metadata
+        return getIconFromDamageForRenderPass(stack.getItemDamage(), pass);
+    }
+
+    /**
+     * Called to get the render key for the stack
+     * <p>
+     * Keep in mind key is prefixed by render type
+     * <p>
+     * Ex: item.inventory.key
+     * item.entity.key
+     *
+     * @param stack - stack being rendered
+     * @return key for the render ID
+     */
+
+    public String getRenderKey(ItemStack stack)
+    {
+        return null;
+    }
+
+    /**
+     * Called to ge the render key for the stack
+     * <p>
+     * Keep in mind key is prefixed by render type
+     * <p>
+     * Ex: item.inventory.key
+     * item.entity.key
+     *
+     * @param stack        - stack being rendered
+     * @param entity       - entity holding the item
+     * @param useRemaining - how many uses are left
+     * @return key for the render ID
+     */
+    public String getRenderKey(ItemStack stack, Entity entity, int useRemaining)
+    {
+        return null;
+    }
+
+    /**
+     * Called to get the icon from the state
+     *
+     * @param data - render data
+     * @param meta - metadata or damage of the item
+     * @param pass - render pass, 0 by default
+     * @return icon, can not be null or will crash
+     */
+    protected IIcon getIconFromState(RenderData data, int meta, int pass)
+    {
+        if (data != null)
+        {
+            //TODO cache list for faster runtime
+            List<String> keys = getIconStateKeys(data, meta, pass);
+
+            //Attempt to get meta
+            for (String key : keys)
+            {
+                IRenderState state = data.getState(key);
+                if (state != null)
+                {
+                    state = data.getState(RenderData.INVENTORY_RENDER_KEY);
+                    if (state != null)
+                    {
+                        IIcon icon = state.getIcon(pass);
+                        if (icon != null)
+                        {
+                            return icon;
+                        }
+                    }
+                }
+            }
+        }
+        return getFallBackIcon();
+    }
+
+    protected List<String> getIconStateKeys(RenderData data, int meta, int pass)
+    {
+        List<String> keys = new ArrayList();
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + meta + "." + pass);
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + meta);
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + pass);
+        keys.add(RenderData.INVENTORY_RENDER_KEY);
+
+        return keys;
+    }
+
+    /**
+     * Called to get a fallback icon to display
+     * when an icon can not be retrieved from
+     * the json render system
+     *
+     * @return icon, can not be null
+     */
+    protected IIcon getFallBackIcon()
+    {
+        return itemIcon != null ? itemIcon : getBlockBase().getIcon(0, 0);
     }
 }
--- src/main/scala/com/builtbroken/mc/seven/framework/block/meta/ItemBlockMeta.java
+++ src/main/scala/com/builtbroken/mc/seven/framework/block/meta/ItemBlockMeta.java
@@ -1,14 +1,19 @@
 package com.builtbroken.mc.seven.framework.block.meta;
 
+import com.builtbroken.mc.client.json.render.RenderData;
+import com.builtbroken.mc.framework.json.imp.IJSONMetaConvert;
 import com.builtbroken.mc.seven.framework.block.ItemBlockBase;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 6/26/2016.
  */
-public class ItemBlockMeta extends ItemBlockBase
+public class ItemBlockMeta extends ItemBlockBase implements IJSONMetaConvert
 {
     /** Quick cache of localization by meta data to provide a small speed bonus */
     public String[] localizationCache = new String[16];
@@ -51,6 +56,23 @@ public class ItemBlockMeta extends ItemBlockBase
         return getUnlocalizedName();
     }
 
+    protected List<String> getIconStateKeys(RenderData data, int meta, int pass)
+    {
+        List<String> keys = new ArrayList();
+        if (meta >= 0 && meta < 16 && getBlockJson().metaDataValues[meta] != null)
+        {
+            keys.add(RenderData.INVENTORY_RENDER_KEY + "." + getBlockJson().metaDataValues[meta].ID + "." + pass);
+            keys.add(RenderData.INVENTORY_RENDER_KEY + "." + getBlockJson().metaDataValues[meta].ID);
+            keys.add(getBlockJson().metaDataValues[meta].ID);
+        }
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + meta + "." + pass);
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + meta);
+        keys.add(RenderData.INVENTORY_RENDER_KEY + "." + pass);
+        keys.add(RenderData.INVENTORY_RENDER_KEY);
+
+        return keys;
+    }
+
     @Override
     public String getUnlocalizedName()
     {
@@ -66,5 +88,18 @@ public class ItemBlockMeta extends ItemBlockBase
     public String toString()
     {
         return "ItemBlock[" + getBlockJson() + "]";
+    }
+
+    @Override
+    public int getMetaForValue(String value)
+    {
+        for (MetaData data : getBlockJson().metaDataValues) //TODO cache in hashmap to improve lookup
+        {
+            if (data.ID.equalsIgnoreCase(value))
+            {
+                return data.index;
+            }
+        }
+        return -1;
     }
 }
