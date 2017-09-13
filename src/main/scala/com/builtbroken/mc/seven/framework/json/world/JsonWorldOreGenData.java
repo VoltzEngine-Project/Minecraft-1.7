052642a42b1c5870e3e40a14b41f20f37e20211a
Robert Seifert <rseifert.phone@gmail.com>
2017-09-12 20:39:39 -0400

    Improved the display and handling of world gen data

--- src/main/scala/com/builtbroken/mc/seven/framework/json/world/JsonWorldOreGenData.java
+++ src/main/scala/com/builtbroken/mc/seven/framework/json/world/JsonWorldOreGenData.java
@@ -2,36 +2,43 @@ package com.builtbroken.mc.seven.framework.json.world;
 
 import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.core.registry.implement.IPostInit;
-import com.builtbroken.mc.lib.helper.LanguageUtility;
+import com.builtbroken.mc.framework.json.debug.IJsonDebugDisplay;
 import com.builtbroken.mc.framework.json.imp.IJsonProcessor;
+import com.builtbroken.mc.framework.json.loading.JsonProcessorData;
 import com.builtbroken.mc.framework.json.processors.JsonGenData;
+import com.builtbroken.mc.lib.helper.LanguageUtility;
 import com.builtbroken.mc.lib.world.generator.OreGenReplace;
 import com.builtbroken.mc.lib.world.generator.OreGeneratorSettings;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
+import java.util.List;
+
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 3/10/2017.
  */
-public class JsonWorldOreGenData extends JsonGenData implements IPostInit
+public class JsonWorldOreGenData extends JsonGenData implements IPostInit, IJsonDebugDisplay
 {
-    Object block;
+    public Object block;
 
-    String oreName;
+    public String key;
 
-    int min_y;
-    int max_y;
+    @JsonProcessorData("type")
+    public String type;
 
-    int branchSize;
-    int chunkLimit;
+    public int min_y;
+    public int max_y;
 
-    public JsonWorldOreGenData(IJsonProcessor processor, Object block, String oreName, int min, int max, int branch, int chunk)
+    public int branchSize;
+    public int chunkLimit;
+
+    public JsonWorldOreGenData(IJsonProcessor processor, Object block, String key, int min, int max, int branch, int chunk)
     {
         super(processor);
         this.block = block;
-        this.oreName = oreName;
+        this.key = key;
         this.min_y = min;
         this.max_y = max;
         this.branchSize = branch;
@@ -44,16 +51,30 @@ public class JsonWorldOreGenData extends JsonGenData implements IPostInit
         ItemStack stack = toStack(block);
         if (stack != null && stack.getItem() instanceof ItemBlock)
         {
-            if (Engine.loaderInstance.getConfig().getBoolean("" + LanguageUtility.capitalizeFirst(oreName) + "_Ore", "WorldGen", true, "Enables generation of the ore in the world"))
+            if (Engine.loaderInstance.getConfig().getBoolean((modName != null ? modName + ":" : "") + LanguageUtility.capitalizeFirst(key) + "_Ore", "WorldGen", true, "Enables generation of the ore in the world"))
             {
-                GameRegistry.registerWorldGenerator(new OreGenReplace(oreName, ((ItemBlock) stack.getItem()).field_150939_a, stack.getItemDamage(), new OreGeneratorSettings(min_y, max_y, chunkLimit, branchSize), "pickaxe", 1), 1);
+                GameRegistry.registerWorldGenerator(new OreGenReplace(((ItemBlock) stack.getItem()).field_150939_a, stack.getItemDamage(), new OreGeneratorSettings(min_y, max_y, chunkLimit, branchSize), "pickaxe", 1), 1);
             }
+        }
+        else
+        {
+            Engine.logger().error("JsonWorldOreGenData: stack '" + stack + "' can not be used for world generation for generator '" + key + "'.");
         }
     }
 
     @Override
     public String getContentID()
     {
-        return null;
+        return key;
+    }
+
+    @Override
+    public void addDebugLines(List<String> lines)
+    {
+        lines.add("Block: " + block);
+        lines.add("Type: " + type);
+        lines.add("Y level: " + min_y + " - " + max_y);
+        lines.add("Branch Size: " + branchSize);
+        lines.add("Chunk Limit: " + chunkLimit);
     }
 }
