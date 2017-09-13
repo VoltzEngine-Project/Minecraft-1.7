cf35b6ffc0a045e15abe4fdccb404a8c8d3b5d7a
Robert Seifert <rseifert.phone@gmail.com>
2017-09-12 20:40:12 -0400

    Changed world gen processor to support ItemJsonEntry and injection keys

--- src/main/scala/com/builtbroken/mc/seven/framework/json/world/JsonWorldOreGenProcessor.java
+++ src/main/scala/com/builtbroken/mc/seven/framework/json/world/JsonWorldOreGenProcessor.java
@@ -19,22 +19,10 @@ import java.util.List;
  */
 public class JsonWorldOreGenProcessor extends JsonProcessor<JsonWorldOreGenData> implements IJsonBlockSubProcessor
 {
-    @Override
-    public String getMod()
-    {
-        return References.DOMAIN;
-    }
 
-    @Override
-    public String getJsonKey()
+    public JsonWorldOreGenProcessor()
     {
-        return "worldGenerator";
-    }
-
-    @Override
-    public String getLoadOrder()
-    {
-        return "block";
+        super(JsonWorldOreGenData.class);
     }
 
     @Override
@@ -43,52 +31,62 @@ public class JsonWorldOreGenProcessor extends JsonProcessor<JsonWorldOreGenData>
         return process(null, -1, null, element);
     }
 
+    /**
+     * Shared processing call
+     *
+     * @param block
+     * @param meta
+     * @param ore
+     * @param element
+     * @return
+     */
     public JsonWorldOreGenData process(Block block, int meta, String ore, JsonElement element)
     {
         JsonObject genData = element.getAsJsonObject();
-        ensureValuesExist(genData, "type", "minY", "maxY", "branchSize", "chunkLimit");
+        ensureValuesExist(genData, "minY", "maxY", "branchSize", "chunkLimit");
 
         //Get ore block
         Object oreBlock;
         if (block == null)
         {
             ensureValuesExist(genData, "block");
-            oreBlock = genData.getAsJsonPrimitive("block").getAsString();
+            oreBlock = getItemFromJson(genData.get("block"));
         }
         else
         {
             oreBlock = new ItemStack(block, 1, meta >= 0 ? meta : 0);
         }
+
+        //get ore name
         String oreName = ore;
         if (oreName == null)
         {
             ensureValuesExist(genData, "oreName");
             oreName = genData.getAsJsonPrimitive("oreName").getAsString();
         }
-        String type = genData.getAsJsonPrimitive("type").getAsString();
+
+        //Load
         int min = genData.getAsJsonPrimitive("minY").getAsInt();
         int max = genData.getAsJsonPrimitive("maxY").getAsInt();
         int branch = genData.getAsJsonPrimitive("branchSize").getAsInt();
         int chunk = genData.getAsJsonPrimitive("chunkLimit").getAsInt();
 
+        //validate
         if (min > 255 || min < 0 || max > 255 || max < 0 || max < min)
         {
             throw new RuntimeException("Min & Max value for ore generators must be between 0-255, and max must be equal to or greater than min");
         }
-
         if (branch <= 0 || chunk <= 0)
         {
             throw new RuntimeException("Branch size per generation and chunk generation limit must be greater than zero.");
         }
 
-        if (type.equalsIgnoreCase("StoneOre"))
-        {
-            return new JsonWorldOreGenData(this, oreBlock, oreName, min, max, branch, chunk);
-        }
-        else
-        {
-            throw new RuntimeException("Failed to ID world ore gen type of " + type);
-        }
+        //Create
+        JsonWorldOreGenData data = new JsonWorldOreGenData(this, oreBlock, oreName, min, max, branch, chunk);
+
+        //Inject
+        processAdditionalKeys(data, genData);
+        return data;
     }
 
     @Override
@@ -109,5 +107,23 @@ public class JsonWorldOreGenProcessor extends JsonProcessor<JsonWorldOreGenData>
         {
             objectList.add(object);
         }
+    }
+
+    @Override
+    public String getMod()
+    {
+        return References.DOMAIN;
+    }
+
+    @Override
+    public String getJsonKey()
+    {
+        return "worldGenerator";
+    }
+
+    @Override
+    public String getLoadOrder()
+    {
+        return "block";
     }
 }
