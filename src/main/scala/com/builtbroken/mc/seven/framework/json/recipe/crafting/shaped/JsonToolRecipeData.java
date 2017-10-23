9e63ad9994d2b21e493b15fa0c98629f75523a8e
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-23 10:55:31 -0400

    Updated tool recipe to use function map for creating tools

--- src/main/scala/com/builtbroken/mc/seven/framework/json/recipe/crafting/shaped/JsonToolRecipeData.java
+++ src/main/scala/com/builtbroken/mc/seven/framework/json/recipe/crafting/shaped/JsonToolRecipeData.java
@@ -1,12 +1,16 @@
 package com.builtbroken.mc.seven.framework.json.recipe.crafting.shaped;
 
 import com.builtbroken.mc.core.Engine;
+import com.builtbroken.mc.core.content.tool.ItemSimpleCraftingTool;
 import com.builtbroken.mc.framework.json.imp.IJsonProcessor;
 import com.builtbroken.mc.framework.recipe.item.RecipeTool;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+
+import java.util.HashMap;
+import java.util.function.Function;
 
 /**
  * Holds onto recipe data until it can be converted.
@@ -16,6 +20,18 @@ import net.minecraft.item.crafting.IRecipe;
  */
 public class JsonToolRecipeData extends JsonShapedRecipeData
 {
+    protected static HashMap<String, Function<String, ItemStack>> toolFactories = new HashMap(); //TODO maybe move to an API for reuse
+
+    static
+    {
+        //TODO load via JSON
+        toolFactories.put("hammer", s -> ItemSimpleCraftingTool.getHammer());
+        toolFactories.put("chisel", s -> ItemSimpleCraftingTool.getChisel());
+        toolFactories.put("drill", s -> ItemSimpleCraftingTool.getDrill());
+        toolFactories.put("cutter", s -> ItemSimpleCraftingTool.getCutters());
+        toolFactories.put("file", s -> ItemSimpleCraftingTool.getFile());
+    }
+
     public JsonToolRecipeData(IJsonProcessor processor, Object output, Object[] data, boolean largeGrid)
     {
         super(processor, output, data, largeGrid);
@@ -42,6 +58,29 @@ public class JsonToolRecipeData extends JsonShapedRecipeData
             Engine.logger().error("The type of output value [" + output + "] could not be recognized for recipe creation. Recipe -> " + this);
         }
         return null;
+    }
+
+    @Override
+    protected Object convert(Object in)
+    {
+        if (in instanceof String)
+        {
+            String value = (String) in;
+            if (value.startsWith("tool@"))
+            {
+                value = value.substring(5, value.length());
+                value = value.toLowerCase().trim();
+                if (toolFactories.containsKey(value))
+                {
+                    ItemStack stack = toolFactories.get(value).apply(value);
+                    if (stack != null && stack.getItem() != null)
+                    {
+                        return stack;
+                    }
+                }
+            }
+        }
+        return convertItemEntry(in);
     }
 
     @Override
