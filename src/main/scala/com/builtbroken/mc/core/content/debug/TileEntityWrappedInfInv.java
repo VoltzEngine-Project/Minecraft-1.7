a682d4bc17905c0111533193874eace35484e9b9
Robert WC Seifert <rseifert.phone@gmail.com>
2018-01-14 04:51:06 -0500

    Moved tile inf wrapper as a temp fix

---
+++ src/main/scala/com/builtbroken/mc/core/content/debug/TileEntityWrappedInfInv.java
@@ -0,0 +1,187 @@
+//=======================================================
+//DISCLAIMER: THIS IS A GENERATED CLASS FILE
+//THUS IS PROVIDED 'AS-IS' WITH NO WARRANTY
+//FUNCTIONALITY CAN NOT BE GUARANTIED IN ANY WAY
+//USE AT YOUR OWN RISK
+//-------------------------------------------------------
+//Built on: Rober
+//=======================================================
+package com.builtbroken.mc.core.content.debug;
+
+import com.builtbroken.mc.api.tile.provider.IInventoryProvider;
+import com.builtbroken.mc.seven.framework.logic.TileEntityWrapper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+
+public class TileEntityWrappedInfInv extends TileEntityWrapper implements IInventoryProvider, ISidedInventory
+{
+	public TileEntityWrappedInfInv()
+	{
+		super(new TileInfInv());
+	}
+
+	//============================
+	//==Methods:ExternalInventory
+	//============================
+
+
+    @Override
+    public IInventory getInventory()
+    {
+        if (tile instanceof IInventoryProvider)
+        {
+            return ((IInventoryProvider) tile).getInventory();
+        }
+        return null;
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int side)
+    {
+        if (getInventory() instanceof ISidedInventory)
+        {
+            return ((ISidedInventory) getInventory()).getAccessibleSlotsFromSide(side);
+        }
+        return new int[0];
+    }
+
+    @Override
+    public boolean canInsertItem(int p_102007_1_, ItemStack p_102007_2_, int p_102007_3_)
+    {
+        if (getInventory() instanceof ISidedInventory)
+        {
+            return ((ISidedInventory) getInventory()).canInsertItem(p_102007_1_, p_102007_2_, p_102007_3_);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canExtractItem(int p_102008_1_, ItemStack p_102008_2_, int p_102008_3_)
+    {
+        if (getInventory() instanceof ISidedInventory)
+        {
+            return ((ISidedInventory) getInventory()).canExtractItem(p_102008_1_, p_102008_2_, p_102008_3_);
+        }
+        return false;
+    }
+
+    @Override
+    public int getSizeInventory()
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().getSizeInventory();
+        }
+        return 0;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot)
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().getStackInSlot(slot);
+        }
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int amount)
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().decrStackSize(slot, amount);
+        }
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot)
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().getStackInSlotOnClosing(slot);
+        }
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack stack)
+    {
+        if (getInventory() != null)
+        {
+            getInventory().setInventorySlotContents(slot, stack);
+        }
+    }
+
+    @Override
+    public String getInventoryName()
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().getInventoryName();
+        }
+        return "inventory";
+    }
+
+    @Override
+    public boolean hasCustomInventoryName()
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().hasCustomInventoryName();
+        }
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit()
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().getInventoryStackLimit();
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player)
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().isUseableByPlayer(player);
+        }
+        return false;
+    }
+
+    @Override
+    public void openInventory()
+    {
+        if (getInventory() != null)
+        {
+            getInventory().openInventory();
+        }
+    }
+
+    @Override
+    public void closeInventory()
+    {
+        if (getInventory() != null)
+        {
+            getInventory().closeInventory();
+        }
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack stack)
+    {
+        if (getInventory() != null)
+        {
+            return getInventory().isItemValidForSlot(slot, stack);
+        }
+        return false;
+    }
+
+}
